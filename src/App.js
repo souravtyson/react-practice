9818448000aa6f6d1191556d1313feaac0244b70
souravtyson <souravtyson@gmail.com>
2023-10-06 05:55:18 +0000

    new component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TextDisplay from './component/TextDisplay';
 import Example from './component/usecallback/exp1/Example';
 import ParentComponent from './component/usecallback/exp2/ParentComponent';
 import ExpInfiniteLoop from './component/useeffect/exp1/ExpInfiniteLoop';
+import Multiply from './component/counter/Multiply.js';
 import './style.css';
 
 export default function App() {
@@ -15,7 +16,8 @@ export default function App() {
       {/* useCallBack hook example */}
       {/* <Example /> */}
       {/* <Example /> */}
-      <ExpInfiniteLoop />
+      {/* <ExpInfiniteLoop /> */}
+      <Multiply />
     </div>
   );
 }
